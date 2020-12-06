b699b99e75c47500edc5a4658d19814356ff5bbe
Ameen <ameenahmed386@gmail.com>
2020-12-06 16:13:06 +0200

    SettingUp Retrofit

---
+++ app/src/main/java/com/ameen/newsapp/data/network/ApiSettings.kt
@@ -0,0 +1,29 @@
+package com.ameen.newsapp.data.network
+
+import com.ameen.newsapp.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiSettings {
+
+    private val retrofit by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+
+    val apiInstance by lazy {
+        retrofit.create(NewsApi::class.java)
+    }
+}
