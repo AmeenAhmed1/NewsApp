b4ef9c61288564827a456c0d779332c9068f4c24
Ameen <ameenahmed386@gmail.com>
2020-12-16 21:32:05 +0200

    Hiding API Key

--- app/src/main/java/com/ameen/newsapp/util/Constants.kt
+++ app/src/main/java/com/ameen/newsapp/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.ameen.newsapp.util
 
+import com.ameen.newsapp.BuildConfig
+
 object Constants {
-    const val API_KEY: String = "23ca3e957d0b4ecab73cf1363592e565"
-    const val BASE_URL:String = "https://newsapi.org/"
+    const val API_KEY: String = BuildConfig.NEWS_API_KEY
+    const val BASE_URL: String = "https://newsapi.org/"
 }
